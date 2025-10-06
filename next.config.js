438c31c3f4e6c18808e02fc393b7677e89e92460
David Miles <dave@modernsitebuilders.com>
2025-10-06 11:02:26 -0400

    new next config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,27 @@ const nextConfig = {
       };
     }
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com",
+              "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://www.googletagmanager.com https://cdn-cookieyes.com https://cloudinary.com https://*.cloudinary.com",
+              "img-src 'self' data: https: blob:",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+              "frame-src 'self'"
+            ].join('; ')
+          }
+        ]
+      }
+    ];
   }
 };
 module.exports = nextConfig;
