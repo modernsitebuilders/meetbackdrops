967f75a2b45430c06bb6788cb22515526e5ab625
David Miles <dave@modernsitebuilders.com>
2025-10-29 14:46:20 -0400

    ugh

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
-import SheetsTracking from '../components/Analytics';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,9 +10,6 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      {/* Google Sheets tracking - THIS WAS MISSING */}
-      <SheetsTracking />
       
       <Script
         id="cookieyes"
