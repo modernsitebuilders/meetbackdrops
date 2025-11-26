41ce0af3b3a43da3db296932b83391f39e902662
David Miles <dave@modernsitebuilders.com>
2025-11-26 13:43:49 -0500

    next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  unoptimized: true,  // Add this line
+  domains: ['streambackdrops.com'],
   formats: ['image/webp', 'image/avif'],
-  deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  deviceSizes: [640, 750, 828, 1080, 1200, 1920],
   imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 },
   
