bea42d20ba109f6a992d2b9437f8f416fcbf78b8
David Miles <dave@modernsitebuilders.com>
2025-11-27 20:38:42 -0500

    fix vercel?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+      unoptimized: true,
   domains: ['streambackdrops.com'],
   formats: ['image/webp', 'image/avif'],
   deviceSizes: [640, 750, 828, 1080, 1200, 1920],
