d17b6713fbc649b76ec0b58eb1ce239792c3a759
David Miles <dave@modernsitebuilders.com>
2026-01-30 13:08:53 -0500

    fixed capptcha via next.config

--- next.config.js
+++ next.config.js
@@ -180,6 +180,21 @@ const nextConfig = {
       }
     ];
   },
+  
+  // Content Security Policy for Turnstile
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://challenges.cloudflare.com https://www.googletagmanager.com https://cdn-cookieyes.com; frame-src 'self' https://challenges.cloudflare.com;"
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
