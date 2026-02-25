e783d1cdb6c6704ca236e3fb9dc4605dfade5d82
David Miles <dave@modernsitebuilders.com>
2026-02-25 13:21:26 -0500

    perfor

--- pages/_app.js
+++ pages/_app.js
@@ -26,12 +26,11 @@ export default function App({ Component, pageProps }) {
         strategy="afterInteractive"
       />
       
-      {/* CookieYes - Changed to afterInteractive with lazyOnload fallback */}
+      {/* CookieYes - Fixed: Only ONE strategy prop, using lazyOnload for best LCP */}
       <Script
         id="cookieyes"
         src="https://cdn-cookieyes.com/client_data/8eb30da506758a4631261b2a/script.js"
-        strategy="afterInteractive"
-        strategy="lazyOnload" // Alternative: use lazyOnload for even lower priority
+        strategy="lazyOnload"
       />
       
       <Script
