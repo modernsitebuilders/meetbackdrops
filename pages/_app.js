4b7104c2c531e04308eda11f5de6b9e0a259fa09
David Miles <dave@modernsitebuilders.com>
2026-02-25 15:39:28 -0500

    finally

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css';
 import Head from 'next/head';
-// import Script from 'next/script';
-// import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
-// import Analytics from '../components/Analytics';
-// import CookieBanner from '../components/CookieBanner';
+import Script from 'next/script';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import Analytics from '../components/Analytics';
 import { useEffect } from 'react';
-// import { getOrCreateSession } from '../lib/sessionTracking';
-
 import { getOrCreateSession } from '../lib/sessionTracking';
 
 export default function App({ Component, pageProps }) {
@@ -20,15 +17,11 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      {/* <Analytics /> */}
-      {/* <Script src="https://challenges.cloudflare.com/turnstile/v0/api.js" strategy="lazyOnload" /> */}
-      {/* <Script src="https://www.googletagmanager.com/gtag/js?id=G-QMD6NEPFWR" strategy="lazyOnload" /> */}
-      {/* <Script id="google-analytics" strategy="lazyOnload">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-QMD6NEPFWR');`}</Script> */}
-      {/* <CookieBanner /> */}
-      
+      <Analytics />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-QMD6NEPFWR" strategy="lazyOnload" />
+      <Script id="google-analytics" strategy="lazyOnload">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-QMD6NEPFWR');`}</Script>
       <Component {...pageProps} />
-      {/* <VercelAnalytics /> */}
+      <VercelAnalytics />
     </>
   );
 }
